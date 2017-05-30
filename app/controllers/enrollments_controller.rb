b3fd8dfedd841a6c1587ced15336aa929354f2f2
Zayn Rahman <zaynrahn@gmail.com>
2017-05-30 18:31:58 +0000

    Added Stripe payment feature

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,23 +2,24 @@ class EnrollmentsController < ApplicationController
 before_action :authenticate_user!
 
   def create
-    current_user.enrollments.create(course: current_course)
+    if current_course.premium?
+      # Amount in cents
+      @amount = (current_course.cost * 100).to_i
 
-    # Amount in cents
-    @amount = 500
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken]
-    )
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Pixter Premium Content',
+        currency: 'usd'
+      )
+    end
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: @amount,
-      description: 'Rails Stripe customer',
-      currency: 'usd'
-    )
-
+  current_user.enrollments.create(course: current_course)
   redirect_to course_path(current_course)
   rescue Stripe::CardError => e
     flash[:error] = e.message
